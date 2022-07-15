17e78ca9557e357fd5cca0cb4fcee4f9aaf5b44a
Duc Thai <ducthai060501@gmail.com>
2022-07-16 00:29:15 +0300

    video constrain and finish

--- main.js
+++ main.js
@@ -26,6 +26,14 @@ const servers = {
   ],
 };
 
+let constrain = {
+  video: {
+    width: {min: 640, ideal: 1920, max: 1920} ,
+    height:{min:480, ideal:1080, max:1080},
+  }, 
+  audio: true 
+}
+
 let init = async () => {
   client = await AgoraRTM.createInstance(APP_ID);
   await client.login({ uid, token });
@@ -41,10 +49,7 @@ let init = async () => {
   client.on("MessageFromPeer", handleMessageFromPeer);
 
   // request media permission
-  localStream = await navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true,
-  });
+  localStream = await navigator.mediaDevices.getUserMedia(constrain);
 
   document.getElementById("user-1").srcObject = localStream;
 };
